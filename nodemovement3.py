12b1a563d349d47250871c41acf2d2eb50333f1f
jclarkrichards <jclarkrichards@gmail.com>
2016-02-04 08:34:25 -0500

    Create nodemovement3.py
    This is the way entities travel from node to node smoothly.  They will travel past a node if there is a neighbor in that direction and no other valid keys are being pressed.  Inherits from the abstract node movement which just has common methods that all forms of node to node movement has.

---
+++ nodemovement3.py
@@ -0,0 +1,43 @@
+import pygame
+from pygame.locals import *
+from vectors import Vector2D
+from nodemovement import *
+
+class FourWayContinuous(FourWayAbstract):
+    def __init__(self, node, entity):
+        '''node is the starting node.  All other nodes are connected
+        entity is the entity that travels from node to node'''
+        FourWayAbstract.__init__(self, node, entity)
+        if self.entity.direction in self.validDirections:
+            self.setEntityDirection(self.entity.direction)
+
+    def update(self, dt):
+        self.moveTowardsTarget(dt)
+        
+        if self.overshotTarget():
+            self.node = self.target
+            self.portal()
+            self.validDirections = self.node.neighbors.keys()
+            if self.direction in self.validDirections:
+                if self.direction != self.entity.direction:
+                    self.entity.position = self.node.position
+                    self.setEntityDirection(self.direction)
+                else:
+                    self.setTarget(self.entity.direction)
+            else:
+                if self.entity.direction in self.validDirections:
+                    self.setTarget(self.entity.direction)
+                else:
+                    self.entity.position = self.node.position
+                    self.entity.direction = STOP
+        else:
+            if self.entity.direction == STOP:
+                if self.direction in self.validDirections:
+                    self.setEntityDirection(self.direction)
+            else:
+                #if self.direction == DIRECTIONS[self.entity.direction] * -1:
+                if self.direction == self.entity.direction*-1:
+                    self.reverseDirection()
+
+    def keyDiscrete(self, key):
+        pass
