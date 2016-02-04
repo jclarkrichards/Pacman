88cf2917ab987accf4369e2907ab800ba7f75133
jclarkrichards <jclarkrichards@gmail.com>
2016-02-04 08:27:10 -0500

    Create modeswitcher.py
    Keeps track of the mode the ghosts should be in.

---
+++ modeswitcher.py
@@ -0,0 +1,21 @@
+from constants import *
+
+class ModeSwitcher(object):
+    def __init__(self):
+        self.setMode(SCATTER)
+        self.modeChange = False
+
+    def update(self, dt):
+        self.timeEllapsed += dt
+        self.modeChange = False
+        if self.mode == SCATTER and self.timeEllapsed >= SCATTERTIME:
+            self.setMode(ATTACK)
+        elif self.mode == ATTACK and self.timeEllapsed >= ATTACKTIME:
+            self.setMode(SCATTER)
+        elif self.mode == FREIGHT and self.timeEllapsed >= FREIGHTTIME:
+            self.setMode(SCATTER)
+
+    def setMode(self, mode):
+        self.mode = mode
+        self.modeChange = True
+        self.timeEllapsed = 0
